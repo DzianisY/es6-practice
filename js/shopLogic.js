1a35e1bbfe943a0c8ae7df91f9816cf4c8bc6af6
Denis Yaroschik <d.yaroschik@atwss.com>
2017-03-07 17:10:06 +0300

    minor syntax improvement

--- js/shopLogic.js
+++ js/shopLogic.js
@@ -9,15 +9,15 @@ const constants = {
     ORDER_PENDING: "pending"
 };
 
-let createCart = (customerName) => {
+const createCart = (customerName) => {
     return new Cart(generateId(), customerName)
 };
 
-let createOrder = (cart) => {
+const createOrder = (cart) => {
     return new Order(generateId(), cart.id, constants.ORDER_PENDING, cart.getSummaryCost)
 };
 
-let resolveOrder = (order, cash) => {
+const resolveOrder = (order, cash) => {
     if (order.summaryPrice<=cash) {
         order.status = constants.ORDER_APPROVED;
         return "Order approved";
@@ -27,7 +27,7 @@ let resolveOrder = (order, cash) => {
 };
 
 
-let generateId = () => {
+const generateId = () => {
     return Math.floor((1 + Math.random()) * 0x10000)
         .toString(16)
         .substring(1);
